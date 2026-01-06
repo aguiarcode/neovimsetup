550dc11f89315a480dabc0884f02c84abbf09454
João <252622789+aguiarcode@users.noreply.github.com>
2026-01-06 01:28:35 +0000

    add leader y/Y to yank to system clipboard
    Added clipboard yank mappings

--- nvim/lua/joao/remap.lua
+++ nvim/lua/joao/remap.lua
@@ -4,3 +4,9 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") 
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+--copy directly to OS clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+
