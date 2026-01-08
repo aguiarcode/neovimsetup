0c73a3b8fc1e30d2c950827d4cc0e812fcd94672
João <252622789+aguiarcode@users.noreply.github.com>
2026-01-08 11:37:47 +0000

    Update remap.lua
    fixed remap duplication

--- nvim/lua/joao/remap.lua
+++ nvim/lua/joao/remap.lua
@@ -7,14 +7,3 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 --copy directly to OS clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
-
-
-
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
-
-
---copy directly to OS clipboard
-vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("n", "<leader>Y", [["+Y]])
-
