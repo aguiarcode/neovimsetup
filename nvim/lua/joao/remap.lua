bf36e20514858563e3c1d78085deddacb7236878
aguiarcode <252622789+aguiarcode@users.noreply.github.com>
2026-01-09 11:01:25 +0000

    tweaked keymaps in remap.lua and sync config changes

--- nvim/lua/joao/remap.lua
+++ nvim/lua/joao/remap.lua
@@ -1,9 +1,18 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv") 
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 
 --copy directly to OS clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+--i am just that petty over 0.5 seconds
+vim.keymap.set("i", "<C-c>", "<Esc>")
+
+vim.keymap.set("n", "Q", "<nop>")
+
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
